de579e7cdea42795e88eea9e323df489c8481ae0
vishnu <130835050+vishnu0004@users.noreply.github.com>
2023-07-14 23:11:21 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,7 @@
+sum = 3
+first = int (input ("enter first number :"))
+third = int (input ("enter third number :"))
+second =int (input ("enter second number :"))
+print("your percentages is :",first+third+second*sum/100)
+
+
